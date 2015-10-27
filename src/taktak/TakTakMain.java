83657202c88ff71f5a66f9fef616133a64864ca1
m-huff <373000822@mvhsrop901-26.vusdnoc.vusd.k12.ca.us>
2015-10-27 10:30:19 -0700

    Initial commit

---
+++ src/taktak/TakTakMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taktak;
+
+/**
+ *
+ * @author 373000822
+ */
+public class TakTakMain {
+
+    public static void main(String[] args) {
+        final TakTakWindow ttw = new TakTakWindow();
+        
+    }
+}
