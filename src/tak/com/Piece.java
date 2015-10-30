b79f2716983602247b7c0997dccd563430a6507b
Cole Poulos <147003480@mvhsrop901-27.vusdnoc.vusd.k12.ca.us>
2015-10-30 10:48:43 -0700

    cleaned up lilwinda and draw method in piece

--- src/tak/com/Piece.java
+++ src/tak/com/Piece.java
@@ -14,7 +14,7 @@ public class Piece {
 	 * Possible front colors - orange, blue, green
 	 * Possible back colors - black, white
 	 */
-	private static ArrayList<Piece> stack = new ArrayList<Piece>();
+	private ArrayList<Piece> stack = new ArrayList<Piece>();
 
 	private int value;
 	private Color backColor;
@@ -40,12 +40,14 @@ public class Piece {
 		value = 0;
 		backColor = Color.white;
 		frontColor = Color.orange;
+                stack.add(this);
 	}
 
 	public Piece(int _value, Color _frontColor, Color _backColor) {
 		value = _value;
 		frontColor = _frontColor;
 		backColor = _backColor;
+                stack.add(this);
 	}
 
 	public void setValue(int _value) {
@@ -95,16 +97,16 @@ public class Piece {
 	}
 
 	public void draw(Graphics2D g, int x, int y) {
-		g.setColor(backColor);
-		g.drawImage((backColor == Color.black ? black.getImage() : white.getImage()), x + 10, y + 8, 75, 75, null);
+		g.setColor(getTopPiece().backColor);
+		g.drawImage((getTopPiece().backColor == Color.black ? black.getImage() : white.getImage()), x + 10, y + 8, 75, 75, null);
 
-		if (!isKing) {
-			g.drawImage((frontColor == Color.orange ? orange.getImage()
-					: frontColor == Color.blue ? blue.getImage() : green.getImage()), x + 10, y + 8, 75, 75, null);
+		if (!getTopPiece().isKing) {
+			g.drawImage((getTopPiece().frontColor == Color.orange ? orange.getImage()
+					: getTopPiece().frontColor == Color.blue ? blue.getImage() : green.getImage()), x + 10, y + 8, 75, 75, null);
 	
 			
 			g.setFont(new Font("Arial Bold", Font.PLAIN, 18));
-			g.drawString(String.valueOf(value), x + 38, y + 53);
+			g.drawString(String.valueOf(getTopPiece().value), x + 38, y + 53);
 		} else {
 			g.drawImage(crown.getImage(), x + 10, y + 8, 75, 75, null);
 		}
@@ -115,14 +117,36 @@ public class Piece {
 
 	public void drawLilWinda(Graphics2D g, int mousex, int mousey) {
 
-		g.setColor(new Color(254, 254, 254, 150));
+		g.setColor(new Color(50,50,50, 150));
 		g.fillRect(mousex, mousey, 100, 150);
 		g.setColor(new Color(50, 50, 50));
 		int xcords[] = {mousex, mousex + 100, mousex + 100, mousex };
 		int ycords[] = {mousey, mousey, mousey + 150, mousey + 150 };
 		g.drawPolyline(xcords, ycords, 4);
-                g.setColor(Color.RED);
+                g.setColor(Color.WHITE);
 		g.setFont(new Font("Arial Bold", Font.BOLD, 11));
 		g.drawString("Total value is "+ value, mousex + 8, mousey+14);
+                g.setFont(new Font("Arial Bold", Font.BOLD, 11));
+                if(stack.size() == 1)
+		g.drawString(stack.size() + " piece in stack", mousex + 8, mousey+145);
+                else
+                g.drawString(stack.size() + " pieces in stack", mousex + 8, mousey+145);
+                
+                int counter = 0;
+                for(Piece temp : stack)
+                { 
+                   g.setColor(temp.backColor);
+                   g.drawImage((temp.backColor == Color.black ? black.getImage() : white.getImage()), mousex+ 30, (mousey+106)-(counter*30), 45, 25, null);
+		   g.drawImage((temp.backColor == Color.black ? black.getImage() : white.getImage()), mousex+ 30, (mousey+100)-(counter*30), 45, 25, null);
+                   
+                   if (!temp.isKing) {
+			g.drawImage((temp.frontColor == Color.orange ? orange.getImage()
+					: temp.frontColor == Color.blue ? blue.getImage() : green.getImage()), mousex+ 30, (mousey+100)-(counter*30), 45, 25, null);
+		   }
+                   else {
+			g.drawImage(crown.getImage(), mousex+ 30, (mousey+100)-(counter*30), 45, 25, null);
+		   }
+                   counter++;
+                }
 	}
 }
