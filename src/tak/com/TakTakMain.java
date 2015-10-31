5c2a90e4d18ad39008ac182174df2acfacc7391d
m-huff <michaelhuff17@gmail.com>
2015-10-31 11:21:43 -0700

    Added background music

--- src/tak/com/TakTakMain.java
+++ src/tak/com/TakTakMain.java
@@ -1,11 +1,17 @@
 package tak.com;
 
+import tak.util.Sound;
 import tak.window.MenuWindow;
 
 public class TakTakMain {
+	
+	//From http://opsound.org/artist/macroform/ - music is public domain
+	static Sound music = new Sound("outoftime.wav");
 
     public static void main(String[] args) {
         final MenuWindow ttw = new MenuWindow();
         
+        if(music.donePlaying == true)
+        	music = new Sound("outoftime.wav");
     }
 }
