08cbdfb92b900ea79bbbeae01d8036ea00833126
m-huff <373000822@mvhsrop901-26.vusdnoc.vusd.k12.ca.us>
2015-11-05 10:17:01 -0800

    Update comments

--- src/tak/com/TakTakMain.java
+++ src/tak/com/TakTakMain.java
@@ -7,11 +7,13 @@ public class TakTakMain {
 	
 	//Music - http://opsound.org/artist/macroform/ - public domain
 	//SFX - http://www.wavsource.com/sfx/sfx.htm - public domain
+        //Logo - http://www.flamingtext.com/net-fu/dynamic.cgi?script=inferno-logo - logo generator
 	
 	//TODO
         // - more sounds
         // - buttons (main menu, network window, after game single player and multi player)
-	
+        // - update rules for new king stuff
+
 	static Sound music = new Sound("darude_sandstorm.wav");
 
     public static void main(String[] args) {
