e1454f4fc3a4a663d736623ce1dc50bf90feb32b
ColePoulos1 <colebsa789@gmail.com>
2015-11-04 21:08:21 -0800

    Cute buttons on home screen

--- src/tak/window/MenuWindow.java
+++ src/tak/window/MenuWindow.java
@@ -11,6 +11,9 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Random;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -40,7 +43,14 @@ public class MenuWindow extends JFrame implements Runnable {
 	public static Random rand = new Random();
 	public static ImageIcon icon = new ImageIcon(MenuWindow.class.getResource("/tak/assets/icon.png"));
 	static ImageIcon background = new ImageIcon(MenuWindow.class.getResource("/tak/assets/wood.png"));
-
+        private static ImageIcon bluebutton = new ImageIcon(MenuWindow.class.getResource("/tak/assets/buttonblue.png"));
+        private static ImageIcon redbutton = new ImageIcon(MenuWindow.class.getResource("/tak/assets/buttonred.png"));
+        
+        private boolean mouseoverAI;
+        private boolean mouseoverClient;
+        private boolean mouseoverServer;
+        private boolean mouseoverRules;
+        
 	private final MenuWindow frame = this;
 
 	public static int textPosition = WINDOW_WIDTH;
@@ -57,26 +67,78 @@ public class MenuWindow extends JFrame implements Runnable {
 		setTitle("Tak-Tak");
 		setLocation(CENTER_X, CENTER_Y);
 
+                addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseMoved(MouseEvent e) {
+                            if(textPosition < 35)
+                                return;
+				int xpos = e.getX();
+				int ypos = e.getY()+2;
+                                
+                                if(xpos >= textPosition-90 && xpos <= textPosition+25 &&
+                                   ypos >= 100 && ypos<= 115)
+                                       mouseoverServer = true;
+                                else
+                                    mouseoverServer = false;
+                                
+                                if(xpos >= textPosition-90 && xpos <= textPosition+20 &&
+                                   ypos >= 140 && ypos<= 155)
+                                       mouseoverClient = true;
+                                else
+                                    mouseoverClient = false;
+                                
+                                if(xpos >= textPosition+68 && xpos <= textPosition+153 &&
+                                   ypos >= 120 && ypos<= 135)
+                                       mouseoverAI = true;
+                                else
+                                    mouseoverAI = false;
+                                
+                                if(xpos >= 475 && xpos <= 530 &&
+                                   ypos >= textPosition+48 && ypos<= textPosition+60)
+                                       mouseoverRules = true;
+                                else
+                                    mouseoverRules = false;
+				repaint();
+			}
+		});
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+                            if(textPosition < 35)
+                                return;
+				if (MouseEvent.BUTTON1 == e.getButton() && mouseoverAI) {
+                                        new TakTakSingleplayerWindow();
+				        frame.dispose();
+                                }
+                                else if (MouseEvent.BUTTON1 == e.getButton() && mouseoverClient) {
+                              		final NetworkWindow rw = new NetworkWindow(true);
+					frame.dispose();
+                                }
+                                else if (MouseEvent.BUTTON1 == e.getButton() && mouseoverServer) {
+ 					final NetworkWindow rw = new NetworkWindow(false);
+					frame.dispose();                                  
+                                }
+                                else if (MouseEvent.BUTTON1 == e.getButton() && mouseoverRules) {
+                                        final RulesWindow rw = new RulesWindow();
+					rw.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					frame.dispose();
+                                }
+                        }
+                });
+                
 		addKeyListener(new KeyAdapter() {
 
 			@SuppressWarnings("static-access")
 			public void keyPressed(KeyEvent e) {
 				if (e.VK_X == e.getKeyCode()) {
-					new TakTakSingleplayerWindow();
-					frame.dispose();
+					
 				}
 				if (e.VK_Z == e.getKeyCode()) {
-					final RulesWindow rw = new RulesWindow();
-					rw.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-					frame.dispose();
+
 				}
 				if (e.VK_C == e.getKeyCode()) {
-					final NetworkWindow rw = new NetworkWindow(true);
-					frame.dispose();
+                                    
 				}
 				if (e.VK_V == e.getKeyCode()) {
-					final NetworkWindow rw = new NetworkWindow(false);
-					frame.dispose();
+					
 				}
 
 				repaint();
@@ -138,11 +200,31 @@ public class MenuWindow extends JFrame implements Runnable {
 		g.setFont(new Font("Arial", Font.BOLD, 42));
 		g.drawString("TAK-TAK", textPosition - 105, 80);
 
-		g.setFont(new Font("Arial", Font.BOLD, 14));
-		g.drawString("Press X to play against an AI", 30, textPosition + 20);
-		g.drawString("Press Z to learn the rules", 30, textPosition);
-		g.drawString("Press C to join a multiplayer game as the client", 30, textPosition + 40);
-		g.drawString("Press V to start a multiplayer game as the server", 30, textPosition + 60);
+//                if(mouseoverServer)
+//                g.drawImage(bluebutton.getImage(), textPosition-105, 100, 105, 30, null);
+//                else
+//                g.drawImage(redbutton.getImage(), textPosition-105, 100, 105, 30, null);
+//                
+//                if(mouseoverClient)
+//                g.drawImage(bluebutton.getImage(), textPosition-105, 140, 105, 30, null);
+//                else
+//                g.drawImage(redbutton.getImage(), textPosition-105, 140, 105, 30, null);
+//                
+//                if(mouseoverAI)
+//                g.drawImage(bluebutton.getImage(), textPosition+40, 120, 105, 30, null);
+//                else
+//                g.drawImage(redbutton.getImage(), textPosition+40, 120, 105, 30, null);
+                
+		g.setFont(new Font("Arial", Font.BOLD, 16));
+                
+                g.setColor(mouseoverRules?Color.red : Color.white);
+                g.drawString("Rules?",475, textPosition +60);
+                g.setColor(mouseoverAI?Color.red : Color.white);
+		g.drawString("Play Vs. AI", 135+62, textPosition +5);
+                g.setColor(mouseoverClient?Color.red : Color.white);
+		g.drawString("Play as Client",135-92, textPosition +25);
+                g.setColor(mouseoverServer?Color.red : Color.white);
+		g.drawString("Play as Server",135-92, textPosition -15);
 
 		gOld.drawImage(image, 0, 0, null);
 	}
