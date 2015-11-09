2211512f4ab8016517e55b8d11dccaed3dee083a
m-huff <373000822@mvhsrop901-26.vusdnoc.vusd.k12.ca.us>
2015-11-09 10:53:31 -0800

    Nevermind

--- src/tak/window/NetworkWindow.java
+++ src/tak/window/NetworkWindow.java
@@ -342,7 +342,7 @@ public class NetworkWindow extends JFrame implements Runnable {
 		for (int i = 0; i < 20; i++) {
 			Color[] colors = {Color.orange, Color.blue, Color.green };
 			Piece p = new Piece((rand.nextInt(4) * 10) + 10, colors[rand.nextInt(colors.length)],
-					isPotentialGameClient ? Color.black : Color.white);
+					rand.nextBoolean() ? Color.black : Color.white);
 			if (rand.nextInt(10) == 0)
 				p.setKing(true);
 			pieces.add(p); 
