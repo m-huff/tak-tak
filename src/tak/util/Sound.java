c31246cac3f172fb470cb7c3b2979db928ae56d5
m-huff <373000822@mvhsrop901-26.vusdnoc.vusd.k12.ca.us>
2015-11-16 10:36:16 -0800

    Fix mute button not turning music back on :neckbeard:

--- src/tak/util/Sound.java
+++ src/tak/util/Sound.java
@@ -34,13 +34,15 @@ public class Sound implements Runnable {
             source.start();
             int read = 0;
             byte[] audioData = new byte[16384];
+            boolean isMuted = TakTakMain.muted;
             while (read > -1) {
-                if (!TakTakMain.muted) {
-                    read = ais.read(audioData, 0, audioData.length);
-                    if (read >= 0) {
-                        source.write(audioData, 0, read);
-                    }
-                }
+            	if (!TakTakMain.muted) {
+	                read = ais.read(audioData, 0, audioData.length);
+	                if (read >= 0) {
+	                    source.write(audioData, 0, read);
+	                }
+            	}
+                isMuted = TakTakMain.muted;
             }
             donePlaying = true;
 
@@ -51,10 +53,10 @@ public class Sound implements Runnable {
             exc.printStackTrace();
         }
     }
-
+    
     public void stop() {
-        myThread.stop();
-        src.drain();
-        src.close();
+    	myThread.stop();
+    	src.drain();
+    	src.close();
     }
 }
